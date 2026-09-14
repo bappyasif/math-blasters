034cb386bb74cfaadf0873a6dcc29c398d5d8f37
bappyasif <bappyasif@hotmail.com>
2026-09-14 18:18:02 +0600

    adjusted codes
    * now that Card and PageLayout is available so making use of it in ModulesList
    * added a bit of css in globals.css for module card description

--- web/src/components/ModulesList.tsx
+++ web/src/components/ModulesList.tsx
@@ -1,26 +1,29 @@
 import { getModules } from "../content"
+import { Card } from "./Card";
+import { PageLayout } from "./PageLayout";
 
 export const ModulesList = () => {
     const modules = getModules();
 
     return (
         modules?.length ? (
-            <div className="modules-list">
-                {modules.map((module) => (
-                    <a 
-                        key={module.slug} 
-                        href={`/modules/${encodeURIComponent(module.slug)}`}
-                        className="module-card-link"
-                    >
-                        {/* will replace it with "CARD" when ready */}
-                        <div>
-                            <h3>{module.title}</h3>
-                            <p>{module.description}</p>
-                            <span>{module.lessons.length} lessons</span>
-                        </div>
-                    </a>
-                ))}
-            </div>
+            <PageLayout heading={<h2>Modules</h2>}>
+                <div className="modules-list">
+                    {modules.map((module) => (
+                        <a
+                            key={module.slug}
+                            href={`/modules/${encodeURIComponent(module.slug)}`}
+                            className="module-card-link"
+                        >
+                            <Card>
+                                <h3 className="card-title">{module.title}</h3>
+                                <p className="module-card-description">{module.description}</p>
+                                <span>{module.lessons.length} lessons</span>
+                            </Card>
+                        </a>
+                    ))}
+                </div>
+            </PageLayout>
         ) : (
             <div className="empty-state">
                 <p>No modules found.</p>
