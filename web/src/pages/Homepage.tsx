eba207317fe117281b097372067663919fa3797d
bappyasif <bappyasif@hotmail.com>
2026-09-16 14:09:24 +0600

    adjusted codes
    * moved PageLayout out of ModulesList to Homepage as suggested
    * adjusted a11y related adjustements in ModulesList to pass all related tests

--- web/src/pages/Homepage.tsx
+++ web/src/pages/Homepage.tsx
@@ -1,7 +1,10 @@
 import { ModulesList } from "../components/ModulesList";
+import { PageLayout } from "../components/PageLayout";
 
 export function Homepage() {
   return (
+    <PageLayout heading={<h1>Modules</h1>}>
       <ModulesList />
+    </PageLayout>
   )
 }
