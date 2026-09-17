3f8ff615f2de598ea30aef29d6aa1916f0c76d98
bappyasif <bappyasif@hotmail.com>
2026-09-17 19:25:33 +0600

    fixing: flagged issues
    * moved example markdown to Homepage for Markdown content default value so that mergewatch dont complain
    * made Markdown to accept content as required prop and only content string is rendered

--- web/src/pages/Homepage.tsx
+++ web/src/pages/Homepage.tsx
@@ -8,9 +8,60 @@ export function Homepage() {
       <ModulesList />
       {
         import.meta.env.DEV && (
-          <RenderMarkdown />   
+          <RenderMarkdown content={exampleMarkdown} />   
         )
       }
     </PageLayout>
   )
 }
+
+const exampleMarkdown = `
+# Lesson example
+
+This is inline math: $3 + 4 = 7$.
+
+- First item
+- Second item
+
+| Name | Value |
+| --- | ---: |
+| Apples | 3 |
+| Oranges | 4 |
+
+\`\`\`js
+const total = 3 + 4;
+\`\`\`
+
+$$
+\\int_0^1 x^2 \\, dx = \\frac{1}{3}
+$$
+
+<script>alert("Not executed")</script>
+
+# Math Blasters
+
+Learn basic math through short tutorials and open-ended labs.
+
+## How to use Math Blasters
+
+1. Click on a topic
+2. Click on a lesson
+3. Click on a tutorial or lab
+4. Solve the exercises
+
+## Topics
+
+- Arithmetic
+- Algebra
+- Geometry
+
+## Credits
+
+- [freeCodeCamp](https://www.freecodecamp.org/learn/)
+- [freeCodeCamp-Summer-Cohort-2026](https://github.com/freeCodeCamp-Summer-Cohort-2026)
+
+## License
+
+[MIT](./LICENSE)
+
+`;
