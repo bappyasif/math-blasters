4f5aadc0f40762de379197564df034df6ff48a43
imsomilg <imsomilg@gmail.com>
2026-09-18 03:50:28 +0530

    style(web): fix markdown spacing and undefined colour token

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,7 @@ import { Layout } from "./components/Layout";
 import { Homepage } from "./pages/Homepage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import FeedbackStyleGuideView from "./pages/FeedbackStyleGuideView";
+import MarkdownStyleGuideView from "./pages/MarkdownStyleGuideView";
 
 /**
  * Route declaration for the app.
@@ -15,6 +16,7 @@ export function AppRoutes() {
       <Route element={<Layout />}>
         <Route index element={<Homepage />} />
         <Route path="/dev-only-feedback-styleguide" element={import.meta.env.DEV ? <FeedbackStyleGuideView /> : <NotFoundPage />} />
+        <Route path="/dev-only-markdown-styleguide" element={import.meta.env.DEV ? <MarkdownStyleGuideView /> : <NotFoundPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
