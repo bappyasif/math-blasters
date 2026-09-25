d396ede4cac55a6206ccac1da155a50cc35e4250
bappyasif <bappyasif@hotmail.com>
2026-09-25 18:07:00 +0600

    fixed lint thrown issue
    * ran `ruff check --fix app/main.py` to fix that

--- api/app/main.py
+++ api/app/main.py
@@ -16,12 +16,12 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import Response
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
 
 from app.config import get_settings
 from app.routers import health
